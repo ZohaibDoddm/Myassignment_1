d57604616b6d91fdbd565bff9b1be67ad416c966
ZohaibDoddm <117536689+ZohaibDoddm@users.noreply.github.com>
2022-11-06 11:36:40 +0530

    Update Mobile.java
    corrected  line 20

--- day1/Mobile.java
+++ day1/Mobile.java
@@ -17,7 +17,7 @@ public class Mobile {
 		System.out.printf("The IMEI number is ", +mob.mobileImeiNumber);
 		System.out.println("The mobile logo is " +mob.mobileLogo);
 		System.out.println("The mobile price is " +mob.noOfMobilePiece);
-		System.out.println("The moile model number is " +mob.modelNumber);
+		System.out.println("The mobile model number is " +mob.modelNumber);
 		System.out.println("The mobile brand name is" +mob.mobileBrandName);
 		System.out.println("The IMEI number is" +mob.mobileImeiNumber);
 		System.out.println("The mobile is damaged ?" +mob.isDamaged);
